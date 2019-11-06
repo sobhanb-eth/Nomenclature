2c9496d6db2313ec51c1745b9ac100c82231b64b
Oguzhan Celik <oguzhan.celik@vipertechnologies.co>
2019-11-06 14:56:12 +0200

    Search in List

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Counter wtf do I need to explain
 counter = int(input("How many Compounds in Formula: "))
+s_counter = counter
 # list for storage of inputs
 listOfInputs = []
 # iterative loop for inputs
@@ -11,10 +12,23 @@ while counter > 0:
     counter = counter - 1
 # list for elements
 Alkali_list = ['H', 'Li', 'Na', 'K', 'Rb', 'Cs', 'Fr']
-AlkalineEarth_list = ['Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra']
-Icosagens_list = ['B', 'Al', 'Ga', 'In', 'Tl']
-Crystal_list = ['C', 'Si', 'Ge', 'Sn', 'Pb']
-Pnicto_list = ['N', 'P', 'As', 'Sb', 'Bi']
-Chalco_list = ['O', 'S', 'Se', 'Te', 'Po']
-Halo_list = ['F', 'Cl', 'Br', 'I', 'At']
-Noble_list = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn']
+AlkalineEarth_list = ['Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra','None']
+Icosagens_list = ['B', 'Al', 'Ga', 'In', 'Tl','None','None']
+Crystal_list = ['C', 'Si', 'Ge', 'Sn', 'Pb','None','None']
+Pnicto_list = ['N', 'P', 'As', 'Sb', 'Bi','Null','Null']
+Chalco_list = ['O', 'S', 'Se', 'Te', 'Po','Null','Null']
+Halo_list = ['F', 'Cl', 'Br', 'I', 'At','Null','Null']
+Noble_list = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn','null']
+
+### TODO
+i = 0
+while i < 8:
+    print("len of input is: ", len(listOfInputs))
+    for x in range(len(Alkali_list)):
+        if listOfInputs[i] == Alkali_list[x]:
+            check = 1
+            print(Alkali_list[x])
+        elif listOfInputs[i] == AlkalineEarth_list[x]:
+            check = 1
+            print(AlkalineEarth_list[x])
+    i += 1
