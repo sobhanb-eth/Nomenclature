aa5818c1a8ca7fca74665619f42142d0bb6522a5
Sobhan Bahrami <sobhan.bahrami@metu.edu.tr>
2019-11-06 21:18:04 +0200

    Add Fucked up Logic to Second Element in List

--- main.py
+++ main.py
@@ -22,29 +22,48 @@ Noble_list = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn', 'null']
 
 ### TODO: Input Analysis
 i = 0
+j = i+2
 x = 0
+y = 0
 # while is_Found == 0:
 for element_list in Alkali_list, AlkalineEarth_list, Icosagens_list, Crystal_list, Pnicto_list, Crystal_list, Halo_list, Noble_list:
     for elem_input in listOfInputs:
         if listOfInputs[i] == Alkali_list[x]:
-            print("Element ", Alkali_list[x], " is in Alkali Group.")
+            if listOfInputs[j] == Alkali_list[y]:
+                print("Element ", Alkali_list[y], " and" " is in Alkali Group.")
+            print("Element ", Alkali_list[x], " and" " is in Alkali Group.")
         elif listOfInputs[i] == AlkalineEarth_list[x]:
+            if listOfInputs[j] == AlkalineEarth_list[y]:
+                print("Element ", AlkalineEarth_list[y], " is in Alkaline Earth Group.")
             print("Element ", AlkalineEarth_list[x], " is in Alkaline Earth Group.")
         elif listOfInputs[i] == Icosagens_list[x]:
+            if listOfInputs[j] == Icosagens_list[y]:
+                print("Element ", Icosagens_list[y], " is in Icosagens Group.")
             print("Element ", Icosagens_list[x], " is in Icosagens Group.")
         elif listOfInputs[i] == Crystal_list[x]:
+            if listOfInputs[j] == Crystal_list[y]:
+                print("Element ", Crystal_list[y], " is in Crystals Group.")
             print("Element ", Crystal_list[x], " is in Crystals Group.")
         elif listOfInputs[i] == Pnicto_list[x]:
+            if listOfInputs[j] == Pnicto_list[y]:
+                print("Element ", Pnicto_list[y], " is in Pnicto Group.")
             print("Element ", Pnicto_list[x], " is in Pnicto Group.")
         elif listOfInputs[i] == Chalco_list[x]:
+            if listOfInputs[j] == Chalco_list[y]:
+                print("Element ", Chalco_list[y], " is in Chalco Group.")
             print("Element ", Chalco_list[x], " is in Chalco Group.")
         elif listOfInputs[i] == Halo_list[x]:
+            if listOfInputs[j] == Halo_list[y]:
+                print("Element ", Halo_list[y], " is in Halogens Group.")
             print("Element ", Halo_list[x], " is in Halogens Group.")
         elif listOfInputs[i] == Noble_list[x]:
+            if listOfInputs[j] == Noble_list[y]:
+                print("Element ", Noble_list[y], " is in Noble Gases Group.")
             print("Element ", Noble_list[x], " is in Noble Gases Group.")
         else:
             print("heyvaaan Yanlis Yaptin bole bisey Yok")
             break
         i += 1
+        j += 1
         break
     break
